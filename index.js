fdbe1a27c0b8a6da90f0cc8d8db69e7e13ca6d93
celinechege <chegeceline18@gmail.com>
2023-03-13 10:26:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    let input = document.querySelector('input#button');
+
+    function clicked(){
+        console.log("Somebody clicked me!");
+    }
+
+    input.addEventListener('click', clicked)
 }
+ addingEventListener();
